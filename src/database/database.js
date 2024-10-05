350c14a3849805444fa143377b3790932dd0d696
Alexanderguzman26 <alqui1996berrio@gmail.com>
2024-10-05 16:46:05 -0500

    add port to database

--- src/database/database.js
+++ src/database/database.js
@@ -4,8 +4,9 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',         
     password: '',         
-    database: 'electiva' 
-});
+    database: 'electiva', 
+    port:3306
+}); 
 connection.connect((err) => {
     if (err) {
         console.error('Error conectando a la base de datos: ' + err.stack);
